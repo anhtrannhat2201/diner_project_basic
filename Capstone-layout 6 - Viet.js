b7cbb57ef685c7517ac1671dcc044c04918eedb2
Trần Nhật Anh <anhtrannhat2201.real@gmail.com>
2023-03-01 10:59:16 +0700

    first commit

---
+++ Capstone-layout 6 - Viet.js
@@ -0,0 +1,17 @@
+// dark theme
+document.getElementById('switchTheme').onclick = function () {
+    document.getElementById("group12").classList.toggle("dark");
+};
+
+// open overlay menu
+document.getElementById('openMenu').onclick = function () {
+    document.getElementById("group12").classList.toggle("open");
+};
+
+// close overlay menu
+document.getElementById('closeOverlay').onclick = function () {
+    document.getElementById("group12").classList.toggle("open");
+};
+
+
+ 
